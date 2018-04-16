8e7ca4f1788c4ecfe8c1703b54c7aecf09884d93
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-04-16 19:46:23 +0700

    'init'

---
+++ tubesSTIMA3.py
@@ -0,0 +1,17 @@
+# Tugas Besar Strategi Algoritma 3
+# Ilham Firdausi Putra - 13516140
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/solution', methods=['POST'])
+def solution():
+    inputKeyword = request.get_json(force=True)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5010)
