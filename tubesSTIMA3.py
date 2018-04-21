2d362e6bfc464e8cfec6f7606d816a7816dd6860
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-04-21 19:00:15 +0700

    fix error tubesSTIMA3

--- tubesSTIMA3.py
+++ tubesSTIMA3.py
@@ -50,7 +50,7 @@ def hello_world():
     algorithm = int(body["algorithm"][0])
     keywordSearch = body["keywordSearch"][0]
 
-    tweetHasil = getTweet(keywordSearch)
+    tweetHasil = getTweets(keywordSearch)
     
     getVerdict(tweetHasil, algorithm, keywordSpam, True)
     return json.dumps({'hasil':tweetHasil})
@@ -89,30 +89,31 @@ def KMP(text, pattern):
     return len(kmp) > 0
 
 ### Boyer-Moore algorithm ###
-def boyer_moore(text, pattern):
-    i = 0
-    while i < len(text):
-        j = len(pattern) - 1
-        while j >= 0:
-            if text[i + j] != pattern[j]:
-                closest_occurence = pattern.rfind(text[i + j])
-                if closest_occurence == -1:
-                    j = len(pattern) - 1
-                    i += j + 1
-                    break
-                elif closest_occurence >= j:
-                    i += 1
-                    j = len(pattern) - 1
-                    break
-                else:
-                    shift = j - closest_occurence
-                    i += shift
-                    j = len(pattern) -1
-                    break
-            j -= 1
-        if j <= -1:
-            return True
-    return False
+def boyer_moore(text, patternList):
+    for pattern in patternList:
+        i = 0
+        while i < len(text):
+            j = len(pattern) - 1
+            while j >= 0:
+                if text[i + j] != pattern[j]:
+                    closest_occurence = pattern.rfind(text[i + j])
+                    if closest_occurence == -1:
+                        j = len(pattern) - 1
+                        i += j + 1
+                        break
+                    elif closest_occurence >= j:
+                        i += 1
+                        j = len(pattern) - 1
+                        break
+                    else:
+                        shift = j - closest_occurence
+                        i += shift
+                        j = len(pattern) -1
+                        break
+                j -= 1
+            if j <= -1:
+                return True
+        return False
 
 
 if __name__ == '__main__':
